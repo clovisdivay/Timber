ee948c1b8fad473ec34fca687eaee1ed8f763b66
clovisdivay <clovisdivay@gmail.com>
2017-12-11 20:13:40 +0000

    Ajoutez des fichiers projet.

---
+++ Timber/Timber.cpp
@@ -0,0 +1,24 @@
+// Timber.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	while (window.isOpen())
+	{
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+			window.close();
+
+		window.clear();
+		window.display();
+	}
+    return 0;
+}
+
